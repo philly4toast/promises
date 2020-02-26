16fab0ba4e86f7ee06892ea0d0975ee88dda468c
philly4toast <phillipyoun@gmail.com>
2020-02-25 19:31:37 -0800

    Started promisification
    Pair with Summer

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,11 +10,31 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filePath, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        var allLine = String(results).split('\n');
+        var firstLine = allLine[0];
+        resolve(firstLine);
+      }
+    });
+  });
   // TODO
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
+  return new Promise(function(resolve, reject) {
+    request(url, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results.statusCode);
+      }
+    });
+  });
   // TODO
 };
 
